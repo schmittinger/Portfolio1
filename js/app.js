347bff7dcf8e1af3cdb938e079a0de1a5ebd62c1
Sascha Schmittinger <sascha-schmittinger@outlook.com>
2020-08-12 11:12:31 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItem = document.querySelectorAll('.nav-item');
+
+
+// set Initial State Of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toogleMenu);
+
+function toogleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItem.forEach(item => item.classList.add('show'));
+
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItem.forEach(item => item.classList.remove('show'));
+
+        showMenu = false;
+    }
+
+}
